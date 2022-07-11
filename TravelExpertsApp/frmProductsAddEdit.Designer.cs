a87b7b033ec7c8d9df563485dc96d81ae90b4628
trevor <treedprosser@gmail.com>
2022-07-11 09:03:47 -0600

    Setup products form

---
+++ TravelExpertsApp/frmProductsAddEdit.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace TravelExpertsApp
+{
+    partial class frmProductsAddEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtProductName);
+            this.groupBox1.Controls.Add(this.lblProductName);
+            this.groupBox1.Controls.Add(this.txtProductID);
+            this.groupBox1.Controls.Add(this.lblProductID);
+            this.groupBox1.Location = new System.Drawing.Point(32, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 207);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Product Information";
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Location = new System.Drawing.Point(6, 42);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(90, 21);
+            this.lblProductID.TabIndex = 0;
+            this.lblProductID.Text = "Product ID: ";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(134, 39);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(236, 29);
+            this.txtProductID.TabIndex = 1;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(6, 115);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(113, 21);
+            this.lblProductName.TabIndex = 2;
+            this.lblProductName.Text = "Product Name:";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(134, 112);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(236, 29);
+            this.txtProductName.TabIndex = 3;
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Location = new System.Drawing.Point(32, 258);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(92, 43);
+            this.btn_OK.TabIndex = 1;
+            this.btn_OK.Text = "&Ok";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(157, 258);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(92, 43);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(430, 29);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowTemplate.Height = 25;
+            this.dgvProducts.Size = new System.Drawing.Size(244, 272);
+            this.dgvProducts.TabIndex = 3;
+            // 
+            // frmProductsAddEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(697, 313);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmProductsAddEdit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add or Modify Products";
+            this.Load += new System.EventHandler(this.frmProductsAddEdit_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox txtProductName;
+        private Label lblProductName;
+        private TextBox txtProductID;
+        private Label lblProductID;
+        private Button btn_OK;
+        private Button btnCancel;
+        private DataGridView dgvProducts;
+    }
+}
---
+++ TravelExpertsApp/frmProductsAddEdit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TravelData;
+
+namespace TravelExpertsApp
+{
+    public partial class frmProductsAddEdit : Form
+    {
+        public Product product;
+        public bool isAdd;
+        private TravelExpertsContext db;
+
+        public frmProductsAddEdit()
+        {
+            InitializeComponent();
+        }
+        private void frmProductsAddEdit_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                db = new TravelExpertsContext();
+            }
+            catch
+            {
+                MessageBox.Show("Error Connecting to the Database", "Database Error");
+                this.Close();
+                return;
+            }
+            
+            if (product == null) // Adding a new product
+            {
+                this.Text = "Add new product";
+                isAdd = true;
+            }
+            else // Modifying a product
+            {
+                this.Text = "Modify existing product";
+                txtProductID.Text = product.ProductId.ToString();
+                txtProductName.Text = product.ProdName;
+            }
+        }
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            if (isAdd)
+            {
+                product = new Product();
+            }
+            db.Dispose();
+            this.Close();
+        }
+    }
+}
