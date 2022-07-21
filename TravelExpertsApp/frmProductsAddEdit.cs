3cf532a75bde36ff795b415e9123f50b46ef002f
trevor <treedprosser@gmail.com>
2022-07-21 08:09:15 -0600

    Refactor/Comment Products Form

--- TravelExpertsApp/frmProductsAddEdit.cs
+++ TravelExpertsApp/frmProductsAddEdit.cs
@@ -13,49 +13,78 @@ namespace TravelExpertsApp
 {
     public partial class frmProductsAddEdit : Form
     {
-        public Product product;
+        // Public data - set from the main form
+        public Product currentProduct;
         public bool isAdd;
-        private TravelExpertsContext db;
 
         public frmProductsAddEdit()
         {
             InitializeComponent();
         }
+
         private void frmProductsAddEdit_Load(object sender, EventArgs e)
         {
-            try
-            {
-                db = new TravelExpertsContext();
-            }
-            catch
-            {
-                MessageBox.Show("Error Connecting to the Database", "Database Error");
-                this.Close();
-                return;
-            }
-            
-            if (product == null) // Adding a new product
-            {
-                this.Text = "Add new product";
-                isAdd = true;
-            }
-            else // Modifying a product
-            {
-                this.Text = "Modify existing product";
-                txtProductID.Text = product.ProductId.ToString();
-                txtProductName.Text = product.ProdName;
-            }
-        }
-        private void btn_OK_Click(object sender, EventArgs e)
-        {
+            // Populate the list of suppliers (if needed)
+            //LoadSuppliers();
+            // Set the title of the form based on the action
             if (isAdd)
             {
-                product = new Product();
-                product.ProductId = Convert.ToInt32(txtProductID.Text);
-                product.ProdName = txtProductName.Text;
+                this.Text = "Add new Product";
             }
-            db.Dispose();
-            this.Close();
+            else
+            {
+                this.Text = "Modify Product";
+                DisplayProduct(); // Display current product (if any)
+            }
+        }
+
+        // Get suppliers from the database and set as the list
+        //private void LoadSuppliers()
+        //{
+        //    try
+        //    {
+        //        using (TravelExpertsContext db = new TravelExpertsContext())
+        //        {
+        //            var suppliers = (from s in db.Suppliers orderby s.SupName select s).ToList();
+        //            cboSuppliers.DataSource = suppliers;
+        //            cboSuppliers.DisplayMember = "SupName";
+        //            cboSuppliers.ValueMember = "SupplierId";
+        //        }
+        //    }
+        //}
+
+        // Displays the current product
+        private void DisplayProduct()
+        {
+            if (currentProduct != null)
+            {
+                txtProductID = currentProduct.ProductId;
+                txtProductName = currentProduct.ProdName;
+                // Add dropdown list for suppliers?
+            }
+            else
+            {
+                MessageBox.Show("There is no product", "Modify error");
+            }
+        }
+
+        // Validate data and set the current product
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            // add if statement for validator
+            if (isAdd)
+            {
+                currentProduct = new Product();
+            }
+            LoadProduct(); // Load new product if add or modify
+        }
+
+        // Load current product with data on the form
+        private void LoadProduct()
+        {
+            currentProduct.ProductId = txtProductID.Text;
+            currentProduct.ProdName = txtProductName.Text;
+            // Add supplier list?
         }
     }
 }
